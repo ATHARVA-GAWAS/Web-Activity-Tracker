af92e535fdf07450e8789bfb449c86197d8dc6db
ATHARVA-GAWAS <atharvagawas21@gmail.com>
2024-05-10 21:59:57 +0530

    Changes

--- backend/myapp/app/controllers/api/users_controller.rb
+++ backend/myapp/app/controllers/api/users_controller.rb
@@ -1,17 +1,20 @@
-class Api::UsersController < ApplicationController
-    def create
-      @user = User.new(user_params)
-      if @user.save
-        render json: { message: 'User created successfully' }, status: :created
-      else
-        render json: { error: 'Failed to create user' }, status: :unprocessable_entity
-      end
-    end
-  
-    private
-  
-    def user_params
-      params.require(:user).permit(:email, :password)
+# app/controllers/api/my_users_controller.rb
+
+class Api::MyUsersController < ApplicationController
+  def create
+    @user = MyUser.new(user_params)
+    if @user.save
+      render json: { user: @user }, status: :created
+    else
+      render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
     end
   end
+
+  private
+
+  def user_params
+    params.require(:my_user).permit(:email, :password)
+  end
+end
+
   
--- backend/myapp/config/routes.rb
+++ backend/myapp/config/routes.rb
@@ -1,5 +1,7 @@
+# config/routes.rb
+
 Rails.application.routes.draw do
   namespace :api do
-    resources :users, only: [:create]
+    resources :my_users, only: [:create]
   end
 end
