2aa679e6e42bbbbe002050675cc006c759853049
ATHARVA-GAWAS <atharvagawas21@gmail.com>
2024-05-10 23:01:49 +0530

    Routes and controllers defined

--- backend/myapp/config/routes.rb
+++ backend/myapp/config/routes.rb
@@ -1,8 +1,17 @@
 # config/routes.rb
 
 Rails.application.routes.draw do
-  devise_for :users
-  resources :website_visits, only: [:create]
+  # Custom routes for user authentication
+  post '/register', to: 'users#register'
+  post '/login', to: 'sessions#login'
+  delete '/logout', to: 'sessions#logout'
+
+  # Endpoint for tracking website visits
+  post '/track_website_visit', to: 'website_visits#create'
+  post '/api/activity', to: 'activity#track'
+  # Endpoint for managing restricted websites
   resources :restricted_websites, only: [:index, :create, :destroy]
+
+  # Endpoint for managing time limits
   resources :time_limits, only: [:index, :create, :destroy]
 end
